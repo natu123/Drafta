5454c82b97f201df195f07fcf9f329f36b6621fe
Lifin <natu.soral.123@gmail.com>
2025-10-01 05:15:11 +0000

    まだ 1から変更できないよ！

--- src/hooks/use-user-preferences.ts
+++ src/hooks/use-user-preferences.ts
@@ -16,9 +16,8 @@ export const useUserPreferences = create<UserPreferencesState>()(
         // Only set the icon if the input is not empty, taking the first character.
         // If the input is empty, do not change the state, allowing the user to clear the input field
         // before typing a new character.
-        const newIcon = icon.charAt(0);
-        if (newIcon) {
-          set({ icon: newIcon });
+        if (icon) {
+          set({ icon: icon.charAt(0) });
         }
       },
     }),
