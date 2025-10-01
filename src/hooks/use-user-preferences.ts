bc1da3d725f7932c27a64bf89b4623ae6fded608
Lifin <natu.soral.123@gmail.com>
2025-10-01 05:14:25 +0000

    まだ 1から変更できないよ！ そもそも 1を削除して 空欄にならないよ！

--- src/hooks/use-user-preferences.ts
+++ src/hooks/use-user-preferences.ts
@@ -13,6 +13,9 @@ export const useUserPreferences = create<UserPreferencesState>()(
     (set) => ({
       icon: '1',
       setIcon: (icon) => {
+        // Only set the icon if the input is not empty, taking the first character.
+        // If the input is empty, do not change the state, allowing the user to clear the input field
+        // before typing a new character.
         const newIcon = icon.charAt(0);
         if (newIcon) {
           set({ icon: newIcon });
