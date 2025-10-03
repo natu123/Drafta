cec861889968e4de81fa4a125da79ae6192936f0
Lifin <natu.soral.123@gmail.com>
2025-10-03 11:42:41 +0000

    > どちらの状態でもシンプルな四角いアイコンに変更します。 違うよ！ Tab Barのアイコンは 戻して！

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import { Bot, FilePlus, Globe, History, Settings, AppWindow } from 'lucide-react';
+import { Bot, FilePlus, Globe, History, Settings, PanelLeft, PanelRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { AppLogo } from '@/components/icons';
 import {
@@ -116,7 +116,7 @@ const Header: React.FC<HeaderProps> = ({
                 onClick={onToggleScreenTab}
                 className="hidden md:flex"
               >
-                <AppWindow className="h-5 w-5" />
+                {isScreenTabActive ? <PanelRight className="h-5 w-5" /> : <PanelLeft className="h-5 w-5" />}
               </Button>
             </TooltipTrigger>
             <TooltipContent>
@@ -182,5 +182,3 @@ const Header: React.FC<HeaderProps> = ({
 };
 
 export default Header;
-
-    
